a00aab270ff73ad53e9e6deffbbe5d0fef5081f3
anismelisa <152842511+anismelisa@users.noreply.github.com>
2023-12-04 15:27:06 +0700

    Delete DB directory

--- DB/MK/aksi_edit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include "../koneksi.php";
-mysqli_query($koneksi,"UPDATE matakuliah SET kode_mk = '$_POST[kode]', nama_mk='$_POST[nama_mk]', sks='$_POST[sks]', semester='$_POST[semester]'  WHERE id= '$_POST[id]'");
-echo"<a href='index.php'>Data Sudah Update...!</a>";
-?>
--- DB/aksi_siswa_edit.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "koneksi.php";
-
-mysqli_query($koneksi, "UPDATE mahasiswa SET nim='$_POST[nim]', alamat='$_POST[alamat]', nama='$_POST[nama]', jenis_kelamin='$_POST[jenis_kelamin]' WHERE id = '$_POST[id]'");
-echo "<a href='siswa.php'>Data Sudah Update...!</a>";
-?>
