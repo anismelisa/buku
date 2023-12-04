eaba9a0503c4daf0d6f96cfa02f923d1d2f6b84e
anismelisa <152842511+anismelisa@users.noreply.github.com>
2023-12-04 15:28:36 +0700

    Delete web directory

--- web/aksi_siswa_input.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "koneksi.php";
-	mysqli_query($koneksi,"INSERT INTO mahasiswa(nim,nama,alamat,jenis_kelamin) 
-	VALUES('$_POST[nim]','$_POST[nama]','$_POST[alamat]','$_POST[jenis_kelamin]')");
-	echo "<a href='siswa.php'>Data Sudah Masuk...!</a>";
-?>
